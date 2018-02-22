e1354efae68c2ceb1da74e94a6552c5bdcc13aa7
横倉達也 <>
2018-02-22 12:45:08 +0900

    view作成

--- src/Model/Entity/Article.php
+++ src/Model/Entity/Article.php
@@ -1,5 +1,4 @@
 <?php
-// src/Model/Entity/Article.php
 namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
---
+++ src/Model/Entity/sample.rb
@@ -0,0 +1,7 @@
+def rename(aaa)
+  name = "Mr.#{aaa}"
+end
+
+name = "sato"
+rename(name)
+puts name
